4989b85aee0967a3caebc32511c81bd80e862b06
Liam Lee <liam.sho.lee1@gmail.com>
2025-06-10 00:11:40 +1000

    changed webpage icon

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -37,7 +36,5 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
